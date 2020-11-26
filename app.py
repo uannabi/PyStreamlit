7449e822fca824bac2f0b3e57f3000593f619be6
uannabi <zahid_sc@hotmail.fr>
2020-11-27 00:36:43 +0600

    pie chart

--- app.py
+++ app.py
@@ -26,22 +26,21 @@ st.sidebar.subheader("Show random tweet")
 
 random_tweet = st.sidebar.radio('Sentiment', ('positive', 'neutral', 'negative'))
 
-st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[["text"]].sample(n=1).iat[0,0])
+st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[["text"]].sample(n=1).iat[0, 0])
 
 st.sidebar.markdown("### number of tweets by sentiment")
 
-select = st.sidebar.selectbox('Visualization type',['Histogram','Pie Chart'],key='1')
+select = st.sidebar.selectbox('Visualization type', ['Histogram', 'Pie Chart'], key='1')
 
 sentiment_count = data['airline_sentiment'].value_counts()
 
-sentiment_count = pd.DataFrame({'Sentiment':sentiment_count.index,'Tweets':sentiment_count.values})
-
+sentiment_count = pd.DataFrame({'Sentiment': sentiment_count.index, 'Tweets': sentiment_count.values})
 
 if not st.sidebar.checkbox("Hide", True):
     st.markdown("### Number of tweets by Sentiment")
     if select == "Histogram":
-        fig = px.bar(sentiment_count,x='Sentiment', y='Tweets',color='Tweets', height = 500)
+        fig = px.bar(sentiment_count, x='Sentiment', y='Tweets', color='Tweets', height=500)
         st.plotly_chart(fig)
-
-
-
+    else:
+        fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
+        st.plotly_chart(fig)
