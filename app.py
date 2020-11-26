5ca3483844e5e62fbb8b7cf15144a776cbc7ed96
uannabi <zahid_sc@hotmail.fr>
2020-11-26 23:55:45 +0600

    cache tweet

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 import streamlit as st
 
-st.title("Hello, world !")
-st.markdown("My Dashboard")
+import pandas as pd
+import numpy as np
+
+st.title('sentiment analysis of tweets about us airlines')
+st.sidebar.title('sentiment analysis of tweets about us airlines')
+
+st.markdown("sentiment of tweet")
+st.sidebar.markdown("sentiment of tweet")
+
+DATA_URL = ('/Users/khoundokarzahid/Stack/github/PyStreamlit/Tweets.csv')
+
+
+@st.cache(persist=True)
+def load_data():
+    data = pd.read_csv(DATA_URL)
+    data['tweet_created'] = pd.to_datetime(data['tweet_created'])
+    return data
+
+
+data = load_data()
