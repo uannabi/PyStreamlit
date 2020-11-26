44d979fe01f2a2c68bc2b3569b56124e73f174f8
uannabi <zahid_sc@hotmail.fr>
2020-11-27 01:05:22 +0600

    multi chart

--- app.py
+++ app.py
@@ -45,24 +45,27 @@ if not st.sidebar.checkbox("Hide", True):
         fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
         st.plotly_chart(fig)
 
-
 # st.map(data)
 
 st.sidebar.subheader("When and where are user tweeting from?")
 hour = st.sidebar.slider("Hours of day", 0, 23)
 modified_data = data[data['tweet_created'].dt.hour == hour]
-if not st.sidebar.checkbox("Close",True,key=1):
+if not st.sidebar.checkbox("Close", True, key=1):
     st.markdown("### Tweets locations based on the time of day")
-    st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data),hour,(hour+1)%24))
+    st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
     st.map(modified_data)
     if st.sidebar.checkbox("Show raw data", False):
         st.write(modified_data)
 
+st.sidebar.subheader("Breakdwon ariline tweets by sentiments")
+choice = st.sidebar.multiselect('Pick airllines',
+                                ('US airways', 'United', 'American', 'Southeast', 'Delta', 'Virgin America'), key='0')
 
 
-
-
-
+if len(choice)>0:
+    choice_data = data[data.airline.isin(choice)]
+    fig_choice = px.histogram(choice_data, x='airline',y='airline_sentiment', histfunc='count',color='airline_sentiment',facet_col= 'airline_sentiment', labels={'airline_sentiment:tweets'},height=600,width=800)
+    st.plotly_chart(fig_choice)
 
 
 
