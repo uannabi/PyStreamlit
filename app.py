47d67906f1a8dde4f575fc101064a5f3f4a43a40
uannabi <zahid_sc@hotmail.fr>
2020-11-27 00:33:05 +0600

    first graph

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
 st.title('sentiment analysis of tweets about us airlines')
 st.sidebar.title('sentiment analysis of tweets about us airlines')
@@ -20,3 +21,27 @@ def load_data():
 
 
 data = load_data()
+
+st.sidebar.subheader("Show random tweet")
+
+random_tweet = st.sidebar.radio('Sentiment', ('positive', 'neutral', 'negative'))
+
+st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[["text"]].sample(n=1).iat[0,0])
+
+st.sidebar.markdown("### number of tweets by sentiment")
+
+select = st.sidebar.selectbox('Visualization type',['Histogram','Pie Chart'],key='1')
+
+sentiment_count = data['airline_sentiment'].value_counts()
+
+sentiment_count = pd.DataFrame({'Sentiment':sentiment_count.index,'Tweets':sentiment_count.values})
+
+
+if not st.sidebar.checkbox("Hide", True):
+    st.markdown("### Number of tweets by Sentiment")
+    if select == "Histogram":
+        fig = px.bar(sentiment_count,x='Sentiment', y='Tweets',color='Tweets', height = 500)
+        st.plotly_chart(fig)
+
+
+
