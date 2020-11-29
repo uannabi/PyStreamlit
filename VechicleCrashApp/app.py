f1f5fbde1822ba2b6a5158d8f066daddd819002a
uannabi <zahid_sc@hotmail.fr>
2020-11-29 12:36:21 +0600

    two app

---
+++ VechicleCrashApp/app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+DATA_URL = ("/Users/khoundokarzahid/Downloads/Motor_Vehicle_Collisions_-_Crashes.csv")
+
+st.title("Motor Vehicle Collision in NYC")
+
+st.markdown("Analysis Motor Vehicle Collision 🚗 in NYC")
+
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
+    lowercase = lambda x: str(x).lower()
+
+    data.rename(lowercase, axis='columns', inplace=True)
+    data.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
+
+    return data
+
+
+data = load_data(100000)
+if st.checkbox("Show Raw Data",False):
+    st.subheader('Raw Data')
+    st.write(data)
