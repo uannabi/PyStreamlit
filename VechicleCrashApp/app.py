48de30775b653118f9b14215762697110c228da8
uannabi <zahid_sc@hotmail.fr>
2020-11-29 16:01:22 +0600

    added 3d chartd

--- VechicleCrashApp/app.py
+++ VechicleCrashApp/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pydeck as pdk
 
 DATA_URL = ("/Users/khoundokarzahid/Downloads/Motor_Vehicle_Collisions_-_Crashes.csv")
 
@@ -20,19 +21,41 @@ def load_data(nrows):
 
     return data
 
+
 data = load_data(100000)
 
 st.header("Where are the most people injured in NYC ?")
-injured_people = st.slider("Number of persons injured in vehicle collision ", 0,19)
-st.map(data.query("injured_persons >= @injured_people")[["latitude","longitude"]].dropna(how="any"))
-
+injured_people = st.slider("Number of persons injured in vehicle collision ", 0, 19)
+st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
 
 st.header("How many collisions occur during a given time  fo day?")
-hour = st.slider("Hour to look at", 0,23)
+hour = st.slider("Hour to look at", 0, 23)
 date = data[data['date/time'].dt.hour == hour]
 
+st.markdown("Vehicle Collisions between %i:00 and %i:00" % (hour, (hour + 1) % 24))
+midpoint = (np.average(data["latitude"]), np.average(data["longitude"]))
+st.write(pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    initial_view_state={
+        "latitude": midpoint[0],
+        "longitude": midpoint[1],
+        "zoom": 11,
+        "pitch": 50,
+    },
+    layers=[
+        pdk.Layer(
+            "HexagonLayer",
+            data=data[['date/time', 'latitude', 'longitude']],
+            get_position=['longitude', 'latitude'],
+            radius=100,
+            extruded=True,
+            pickable=True,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+        )
+    ]
+))
 
-
-if st.checkbox("Show Raw Data",False):
+if st.checkbox("Show Raw Data", False):
     st.subheader('Raw Data')
     st.write(data)
