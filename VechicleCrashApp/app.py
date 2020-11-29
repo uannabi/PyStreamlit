9278afabff7fa3cb403b89b0c53b1d0e0039c4f8
uannabi <zahid_sc@hotmail.fr>
2020-11-29 15:23:22 +0600

    shwoing the app

--- VechicleCrashApp/app.py
+++ VechicleCrashApp/app.py
@@ -20,8 +20,14 @@ def load_data(nrows):
 
     return data
 
-
 data = load_data(100000)
+
+st.header("Where are the most people injured in NYC ?")
+injured_people = st.slider("Number of persons injured in vehicle collision ", 0,19)
+st.map(data.query("injured_persons >= @injured_people")[["latitude","longitude"]].dropna(how="any"))
+
+
+
 if st.checkbox("Show Raw Data",False):
     st.subheader('Raw Data')
     st.write(data)
