6b8b45dbb02d7fc3a9e238b8d930076fdc4268b1
uannabi <zahid_sc@hotmail.fr>
2020-12-03 22:58:47 +0600

    custome map

--- WifiData/app.py
+++ WifiData/app.py
@@ -6,7 +6,10 @@ import plotly.express as px
 
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
+import pydeck as pdk
+import plotly.express as px
 
+DATE_TIME = "date/time"
 st.title('Bangladesh WiFi Data Analysis')
 st.sidebar.title('WiFi Data Analysis')
 
@@ -29,11 +32,44 @@ def load_data(nrows):
 data = load_data(100000)
 data[['latitude', 'longitude']].to_csv('lat_lng.csv', index=False)
 st.sidebar.subheader("Where People ar the most WiFI used ?")
-wifi_user = st.sidebar.slider('Number of Wifi user in Bangladesh',1,1000)
+wifi_user = st.sidebar.slider('Number of Wifi user in Bangladesh', 1, 1000)
 st.map(data.query("brq_count >= @wifi_user")[['latitude', 'longitude']].dropna(how="any"))
 if st.sidebar.checkbox("Show Raw Data", False):
     st.subheader("Raw Data")
     st.write(data)
+
+# st.sidebar.header("When wifi user are seen ?")
+# hour = st.sidebar.slider("Hour to look at", 0, 23)
+# date = data[data['last_seen'].dt.hour == hour]
+# original_data = data
+#
+# data = data[data[DATE_TIME].dt.hour == hour]
+# st.sidebar.markdown("user last seen between %i:00 and %i:00" % (hour, (hour + 1) % 24))
+
+midpoint = (np.average(data["latitude"]), np.average(data["longitude"]))
+st.write(pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    initial_view_state={
+        "latitude": midpoint[0],
+        "longitude": midpoint[1],
+        "zoom": 11,
+        "pitch": 50,
+    },
+    layers=[
+        pdk.Layer(
+            "HexagonLayer",
+            data=data[['last_seen', 'latitude', 'longitude']],
+            get_position=['longitude', 'latitude'],
+            auto_highlight=True,
+            radius=100,
+            extruded=True,
+            pickable=True,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+        ),
+    ],
+))
+
 # wifi_options = st.sidebar.radio('con_type',('Cable/DSL','Corporate','Dialup'))
 # st.sidebar.markdown(data.query('con_type == @wifi_options'))
 # ['brq_count'].sample(n=1).iat[0,0]
